99fe63daf433f36957da7949cad1d30a14239562
cr7pt0 <cryptoface-15@yahoo.com>
2015-06-10 16:12:07 -0400

    Create ecc_sign_and_verify.sh

---
+++ test/ecc_sign_and_verify.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ 
+if [ $# -eq 0 ]; then
+echo "Usage: $0 verify [file to sign]";
+exit 1;
+fi
+
+public="$(eclet get-pub)"
+signature="$(eclet sign -f testfile)"
+
+eclet verify -f $1 --signature $signature --public-key $public -v | grep -i "verify success"
