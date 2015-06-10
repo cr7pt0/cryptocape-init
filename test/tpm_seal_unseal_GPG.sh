6959f25b09ab7de3f4db814db2194fa0e5451569
cr7pt0 <cryptoface-15@yahoo.com>
2015-06-10 01:25:24 -0400

    Update tpm_seal_unseal_GPG.sh

--- test/tpm_seal_unseal_GPG.sh
+++ test/tpm_seal_unseal_GPG.sh
@@ -4,4 +4,5 @@ tpm_sealdata -p 9 -i secring.gpg -o sealed -z -l debug
 echo Displaying Encrypted GPG
 cat sealed
 tpm_unsealdata -i sealed -o unsealed -z
+echo Displaying Decrypted GPG
 cat unsealed
