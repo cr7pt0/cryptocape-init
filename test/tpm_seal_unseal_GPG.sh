22e6c22c17ccb795fd1e62e51e285f829bf242e3
cr7pt0 <cryptoface-15@yahoo.com>
2015-06-10 01:24:33 -0400

    Create tpm_seal_unseal_GPG.sh

---
+++ test/tpm_seal_unseal_GPG.sh
@@ -0,0 +1,7 @@
+echo Displaying Decrypted GPG
+cat secring.gpg
+tpm_sealdata -p 9 -i secring.gpg -o sealed -z -l debug
+echo Displaying Encrypted GPG
+cat sealed
+tpm_unsealdata -i sealed -o unsealed -z
+cat unsealed
