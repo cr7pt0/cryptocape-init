0b8f7b217059fe0044ebab5e92df4d1a6ff6092f
cr7pt0 <cryptoface-15@yahoo.com>
2015-06-10 16:18:47 -0400

    Update ecc_sign_and_verify.sh

--- test/ecc_sign_and_verify.sh
+++ test/ecc_sign_and_verify.sh
@@ -8,4 +8,4 @@ fi
 public="$(eclet get-pub)"
 signature="$(eclet sign -f testfile)"
 
-eclet verify -f $1 --signature $signature --public-key $public -v | grep -i "verify success"
+eclet verify -f $1 --signature $signature --public-key $public -v | grep -i "verify success|failure"
